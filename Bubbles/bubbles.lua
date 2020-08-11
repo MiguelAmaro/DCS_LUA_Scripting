0f7bc18937efd2ed8d01f0bce3c539e0b5e368f9
MiguelAmaro <miguelamaro100@gmail.com>
2020-08-10 22:26:30 -0400

    added slots which are linked dynamically to client events restored enemy spawn logic

--- Bubbles/bubbles.lua
+++ Bubbles/bubbles.lua
@@ -1,29 +1,28 @@
 --***************************
 -- Variables
 --***************************
--- Player
+-- Airwing
 --local playerLandAirwing = AIRWING:New("Warehouse Batumi", "Player Land Fighter Wing")
 --playerLandAirwing:Start()
-
-local player = CLIENT:FindByName("Player DefensivePerimeter F-16")
-player:HandleEvent( EVENTS.Crash )
-
-
 --playerLandAirwing:NewPayload()
+
+
+-- Player
+local player = nil
+
 
 -- Friends
 local friendCarrierGroup = GROUP:FindByName("Friendly Boats")
 
 
-
 -- Enemy
 local firstEnemy = true
 local enemyPlane = SPAWN:New( "Enemy BFM #001" )
-                        :InitLimit(1, 5)
-                        :InitRandomizePosition( true, 200, 50 )
-                        :SpawnScheduled( 5, .5 )
+                        --:InitLimit(1, 5)
+                        --:InitRandomizePosition( true, 200, 50 )
+                        --:SpawnScheduled( 5, .5 )
                         
-enemyPlane:HandleEvent( EVENTS.Crash )
+enemyPlane:HandleEvent( EVENTS.Kill )
       
 local enemyNum       = 1
 local enemyTemplates = {"Enemy BFM #001"       ,
@@ -33,16 +32,22 @@ local enemyTemplates = {"Enemy BFM #001"       ,
                         "Enemy BFM #005"       ,
                         "Enemy BFM #006"
                        }
+ 
+ -- Database
+ local databaseHandle = DATABASE:New()
+ databaseHandle:HandleEvent(EVENTS.PlayerEnterUnit)
                        
 -- Zone Section
 local zones = {--zoneBFM = ZONE_RADIUS:New("zoneBFM")
               }
+local currentZone = nil
+local inTransit   = true
 
 local zoneHomeAirBase        = ZONE      :New("zoneHomeAirBase"       )
 local zoneAntiShip           = ZONE      :New("zoneAntiShip"          )
 local zoneBFM                = ZONE      :New("zoneBFM"               )
 local zoneBombing            = ZONE      :New("zoneBombing"           )
-local zoneHomeCarrier        = ZONE_GROUP:New("zoneHomeCarrier", friendCarrierGroup, 20000)
+local zoneHomeCarrier        = ZONE_GROUP:New("zoneHomeCarrier", friendCarrierGroup, 18000)
 local zoneDefensivePerimeter = ZONE      :New("zoneDefensivePerimeter")
 
 
@@ -57,35 +62,37 @@ local Music_zoneDefensivePerimeter = USERSOUND:New("afdx_DefensivePerimeter.wav"
 ---- Logic Music
 local Music_MissionSuccess         = USERSOUND:New("afdx_MissionCompleted.wav"  )
 local Music_playerDied             = USERSOUND:New("afdx_MissionFailed.wav"     )
-local Music_inTransit              = USERSOUND:New("afdx_InTransit.wav"            )
-
--- Schedulers
-
-
-                                 
+local Music_inTransit              = USERSOUND:New("afdx_InTransit.wav"         )
+                              
 --***************************
 -- Function Definitions
 --***************************
 function SpawnEnemyPlane( newEnemyPlane )
+  if enemyNum < 5 then
+    enemyPlane = SPAWN:New( enemyTemplates[enemyNum] )
+    enemyNum   = enemyNum + 1
+  else
+    Music_MissionSuccess:ToAll();
+    enemyNum   = 0
+  end
   enemyPlane:Spawn()
-  enemyNum = enemyNum + 1
 end
 
-local currentZone = nil
-local inTransit   = true
-
 function ScanZones()
-  if     player:IsInZone(zoneHomeAirBase         ) then currentZone = zoneHomeAirBase
-  elseif player:IsInZone(zoneAntiShip            ) then currentZone = zoneAntiShip
-  elseif player:IsInZone(zoneBFM                 ) then currentZone = zoneBFM
-  elseif player:IsInZone(zoneBombing             ) then currentZone = zoneBombing
-  elseif player:IsInZone(zoneHomeCarrier         ) then currentZone = zoneHomeCarrier
-  elseif player:IsInZone(zoneDefensivePerimeter  ) then currentZone = zoneDefensivePerimeter
-  else   currentZone = nil 
+  if player ~= nil then
+    if     player:IsInZone(zoneHomeAirBase         ) then currentZone = zoneHomeAirBase
+    elseif player:IsInZone(zoneAntiShip            ) then currentZone = zoneAntiShip
+    elseif player:IsInZone(zoneBFM                 ) then currentZone = zoneBFM
+    elseif player:IsInZone(zoneBombing             ) then currentZone = zoneBombing
+    elseif player:IsInZone(zoneHomeCarrier         ) then currentZone = zoneHomeCarrier
+    elseif player:IsInZone(zoneDefensivePerimeter  ) then currentZone = zoneDefensivePerimeter
+    else   currentZone = nil 
+    end
+  
+    ZoneEntryAndExitHandler(currentZone)
+  else
+    MESSAGE:New("Player Var is nil" , 1,   "Debubg"):ToAll()
   end
-  
-  ZoneEntryAndExitHandler(currentZone)
-  
 end
 
 function ZoneEntryAndExitHandler(currentZone) 
@@ -151,18 +158,29 @@ zoneDefensivePerimeter :SmokeZone( SMOKECOLOR.Red    , 90 )
 
 MESSAGE:New("Welcome to the Bubbles Mission", 10, "Start Message 1"):ToAll()
 
+Music_inTransit:ToAll()
+
+--*************************
+-- Schedulers
+--*************************
 zoneScanScheduler = SCHEDULER:New(nil, ScanZones, {}, 0, 2)
 
 --*************************
 -- Event Handlers
 --*************************
-function enemyPlane:OnEventCrash( EventData )
+function enemyPlane:OnEventKill         ( EventData )
     MESSAGE:New("You killed that son of a bitch!", 10, "Success"):ToAll()
     MESSAGE:New(   "Fuck here comes another one!", 10,   "Brief"):ToAll()
-    Music_MissionSuccess:ToAll();
+    SpawnEnemyPlane()
 end  
 
-function player:OnEventCrash( EventData )
+function playerCrashEventHandler     ( EventData )
   player:MessageToAll("You died!", 10, "Failed")
   Music_playerDied:ToAll();
- end
+end
+
+function databaseHandle:OnEventPlayerEnterUnit ( EventData )
+  player = EventData.IniUnit
+  player:MessageToAll("Current Unit : "  .. EventData.IniUnitName, 15,   "Debug")
+  player:HandleEvent (EVENTS.Crash, playerCrashEventHandler)
+end
